ecb235d658444a741520337aab1d4e7290cc7f75
machewgc <mattygcz@gmail.com>
2016-07-05 23:26:06 -0500

    added logic and passed test for letters Q and Z

--- java/scrabble/src/main/java/Scrabble.java
+++ java/scrabble/src/main/java/Scrabble.java
@@ -9,15 +9,17 @@ public class Scrabble {
     char[] charsInput= stringInput.toCharArray();
     Integer score = 0;
     for (Integer i = 0; i < stringInput.length(); i++) {
-      if ( charsInput[i] == 'j' || charsInput[i] == 'x' ) {
+      if (Pattern.matches("[qzQZ]", Character.toString(charsInput[i]))) {
+        score += 10;
+      } else if ( charsInput[i] == 'j' || charsInput[i] == 'x' ) {
         score += 8;
       } else if ( charsInput[i] == 'k') {
         score += 5;
-      } else if (Pattern.matches("[fhvwy]", Character.toString(charsInput[i]))) {
+      } else if (Pattern.matches("[fhvwyFHVWY]", Character.toString(charsInput[i]))) {
         score += 4;
-      } else if (Pattern.matches("[bcmp]", Character.toString(charsInput[i]))) {
+      } else if (Pattern.matches("[bcmpBCMP]", Character.toString(charsInput[i]))) {
         score += 3;
-      } else if (Pattern.matches("[gd]", Character.toString(charsInput[i]))) {
+      } else if (Pattern.matches("[gdGD]", Character.toString(charsInput[i]))) {
         score += 2;
       } else {
         score += 1;
