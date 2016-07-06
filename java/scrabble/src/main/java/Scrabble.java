237a8de61f18f39f78c7a082ed563452654fd25a
machewgc <mattygcz@gmail.com>
2016-07-05 23:22:06 -0500

    added logic for letters J and X, passed test

--- java/scrabble/src/main/java/Scrabble.java
+++ java/scrabble/src/main/java/Scrabble.java
@@ -9,7 +9,9 @@ public class Scrabble {
     char[] charsInput= stringInput.toCharArray();
     Integer score = 0;
     for (Integer i = 0; i < stringInput.length(); i++) {
-      if ( charsInput[i] == 'k') {
+      if ( charsInput[i] == 'j' || charsInput[i] == 'x' ) {
+        score += 8;
+      } else if ( charsInput[i] == 'k') {
         score += 5;
       } else if (Pattern.matches("[fhvwy]", Character.toString(charsInput[i]))) {
         score += 4;
