b185bbdf133f095bb16d04d0a4348be404d45683
machewgc <mattygcz@gmail.com>
2016-07-06 21:15:32 -0500

    added test for 20, 30, 40, 50, 60, 70, 80, and 90

--- java/numbers-to-words/src/test/java/NumbersToWordsTest.java
+++ java/numbers-to-words/src/test/java/NumbersToWordsTest.java
@@ -9,5 +9,9 @@ public class NumbersToWordsTest {
     assertEquals("fifteen", numbersToWordsTest.numbersToWords(15));
   }
 
-
+  @Test
+  public void numbersToWords_Numbers20_30_40_50_60_70_80_90_SingleCorrespondingWord() {
+    NumbersToWords numbersToWordsTest = new NumbersToWords();
+    assertEquals("thirty", numbersToWordsTest.numbersToWords(30));
+  }
 }
