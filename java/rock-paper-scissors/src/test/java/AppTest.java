486b80551c3e13c5606b00d9d78c264f9c741238
machewgc <mattygcz@gmail.com>
2016-07-12 23:20:16 -0500

    passed selenium test for player 2 human victory

--- java/rock-paper-scissors/src/test/java/AppTest.java
+++ java/rock-paper-scissors/src/test/java/AppTest.java
@@ -32,18 +32,20 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Player 1 wins with rock");
   }
 
-  /*@Test
+  @Test
   public void playerTwoWins() {
     goTo("http://localhost:4567");
     Select choiceOne = new Select(webDriver.findElement(By.id("choiceOne")));
     Select choiceTwo = new Select(webDriver.findElement(By.id("choiceTwo")));
+    Select opponent = new Select(webDriver.findElement(By.id("opponent")));
     choiceOne.selectByValue("rock");
     choiceTwo.selectByValue("paper");
+    opponent.selectByValue("human");
     submit(".btn");
     assertThat(pageSource()).contains("Player 2 wins with paper");
   }
 
-  @Test
+  /*@Test
   public void nobodyWins() {
     goTo("http://localhost:4567");
     Select choiceOne = new Select(webDriver.findElement(By.id("choiceOne")));
