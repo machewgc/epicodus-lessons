e8252ea82ea9b9d43d180e03d3ed0426b6c3ae34
machewgc <mattygcz@gmail.com>
2016-07-17 20:45:39 -0500

    added logic for more than 1 penny, passed countCoins test

--- java/make-change/src/main/java/MakeChange.java
+++ java/make-change/src/main/java/MakeChange.java
@@ -1,6 +1,8 @@
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.lang.Number;
 
 public class MakeChange {
   public static void main(String[] args) {
@@ -8,6 +10,15 @@ public class MakeChange {
   }
 
   public static String countCoins(BigDecimal amount) {
-    return "1 penny";
+    BigDecimal cent = new BigDecimal(0.01);
+    BigDecimal count = amount.divide(cent,0,BigDecimal.ROUND_HALF_UP);
+
+
+    if (count.intValue() > 1) {
+      return String.format("%d pennies", count.intValue());
+    } else {
+      return String.format("%d penny", count.intValue());
+    }
+
   }
 }
