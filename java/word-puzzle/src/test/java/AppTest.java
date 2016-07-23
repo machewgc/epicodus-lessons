2aaf8fdee2265d8a97584ecca7eeecc47bdc287e
machewgc <mattygcz@gmail.com>
2016-07-23 17:19:49 -0500

    Added Selenium test for wrong guess from user, passed

--- java/word-puzzle/src/test/java/AppTest.java
+++ java/word-puzzle/src/test/java/AppTest.java
@@ -21,31 +21,29 @@ public class AppTest extends FluentTest {
   public static ServerRule server = new ServerRule();
 
   @Test
-  public void browserDisplaysPuzzledPhrase() {
+  public void browserDisplaysPuzzledPhraseCorrectAnswerFromUser() {
     goTo("http://localhost:4567");
     WebElement phraseToTest = webDriver.findElement(By.id("wordOrPhrase"));
     phraseToTest.sendKeys("cucumber SOCKS");
     submit(".btn");
     assertThat(pageSource()).contains("c-c-mb-r S-CKS");
-    /*WebElement answer = webDriver.findElement(By.id("answer"));
-    assertEquals(answer,"cucumber SOCKS");
     WebElement guess = webDriver.findElement(By.id("guess"));
     guess.sendKeys("cucumber SOCKS");
-    submit(".btn");
-    assertThat(pageSource()).contains("Congrats!");*/
-  }
-
-  /*@Test
-  public void correctPhraseVictory() {
-    goTo("http://localhost:4567/puzzle");
-    WebElement puzzle = webDriver.findElement(By.id("puzzle"));
-    puzzle.sendKeys("cucumber SOCKS");
-    WebElement guess = webDriver.findElement(By.id("guess"));
-    guess.sendKeys("cucumber SOCKS");
-    WebElement answer = webDriver.findElement(By.id("answer"));
-    answer.sendKeys("cucumber SOCKS");
     submit(".btn");
     assertThat(pageSource()).contains("Congrats!");
-  }*/
+  }
+
+  @Test
+  public void wrongAnswerFromUser() {
+    goTo("http://localhost:4567");
+    WebElement phraseToTest = webDriver.findElement(By.id("wordOrPhrase"));
+    phraseToTest.sendKeys("cucumber SOCKS");
+    submit(".btn");
+
+    WebElement guess = webDriver.findElement(By.id("guess"));
+    guess.sendKeys("cucumber OCKS");
+    submit(".btn");
+    assertThat(pageSource()).contains("Sorry!");
+  }
 
 }
